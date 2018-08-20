5be6d8aa6fa1026bffc628b7eb5be9d59fe722df
GisellePerez <giselleperez91@gmail.com>
2018-08-20 13:47:46 -0300

    user points in progress2

--- api/controllers/pointsController.js
+++ api/controllers/pointsController.js
@@ -1,20 +1,38 @@
 const self = {};
 const axios = require ('axios');
 
+// const BASE_URL = "https://aerolab-challenge.now.sh"
+// const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Yjc3NGEwMWFkMGE5MzAwNWI3YzRjNDEiLCJpYXQiOjE1MzQ1NDQzODV9.Vp5XnQ37oxa-vXuORjSbFqEsQVwu7Mpk_31ONoxX8pA';
+
+// const DEFAULT_OPTIONS = {
+//     crossDomain: true,
+//     responseType: "json",
+//     headers: {
+//         Authorization: `Bearer ${TOKEN}`,
+//         "Content-Type": "application/json"
+//     }
+// }
+
+// const self = {};
+// const axios = require ('axios');
+
 const BASE_URL = "https://aerolab-challenge.now.sh"
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Yjc3NGEwMWFkMGE5MzAwNWI3YzRjNDEiLCJpYXQiOjE1MzQ1NDQzODV9.Vp5XnQ37oxa-vXuORjSbFqEsQVwu7Mpk_31ONoxX8pA';
 
-const DEFAULT_OPTIONS = {
-    crossDomain: true,
-    responseType: "json",
-    headers: {
-        Authorization: `Bearer ${TOKEN}`,
-        "Content-Type": "application/json"
-    }
-}
 
 self.getMorePoints = function(req,res,next) {
-    console.log('points')
+    
+    fetch(`${BASE_URL}/user/points`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${TOKEN}`
+        },
+        body: JSON.stringify({ 'amount': 1000 })
+    })
+
 }
 
-module.exports = self; 
+
+module.exports = self;
